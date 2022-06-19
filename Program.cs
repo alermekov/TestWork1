c4ee34c6567c991baf048bfcf55f695eda97ce34
Aleksandr Ermekov <al-ermekov@yandex.ru>
2022-06-19 23:20:24 +0300

    Program and files changes added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+string[] input = new string[] {"hello", "2", ":-)"};
+
+int temp = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3) temp++;
+}
+
+string[] output = new string[temp];
+int j = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        output[j] = input[i];
+        j++;
+    }
+    
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", output));
